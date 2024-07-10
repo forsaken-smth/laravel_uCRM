03e77ec93b38c18210c2b964fd69f7e8a47b6556
forsaken-smth <onigiri19194545@gmail.com>
2024-07-10 18:04:02 +0900

    inertia-vue.js

---
+++ resources/js/Pages/ComponentTest.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { reactive, ref } from 'vue'
+import GuestLayout from '@/Layouts/GuestLayout.vue'
+import Label from '@/Components/InputLabel.vue'
+import Input from '@/Components/TextInput.vue'
+
+const model = ref('')
+function output(){
+    console.log(model.value)
+}
+
+</script>
+
+<template>
+    <GuestLayout>
+    <Label value="件名">タイトル</Label>
+    <Input modelValue="初期値が入ります。" v-model="model" @input="output"></Input>
+    </GuestLayout>
+</template>
